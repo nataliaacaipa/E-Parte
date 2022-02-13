dd650989883a770cdd9dd19dc6958e13516c86cb
Dani <daniel.gonzalez@immune.institute>
2022-02-13 18:10:08 +0100

    Nuevas Pantallas y cambio de texto los labels.

--- app/src/main/java/institute/immune/e_parte/Home.java
+++ app/src/main/java/institute/immune/e_parte/Home.java
@@ -2,13 +2,32 @@ package institute.immune.e_parte;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Home extends AppCompatActivity {
-
+    private Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button:
+                intent = new Intent(this,MenuActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.button2:
+                intent = new Intent(this,RegisterActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.button3:
+                intent = new Intent(this,CodeActivity.class);
+                startActivity(intent);
+        }
     }
 }
