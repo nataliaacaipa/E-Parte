84d937b91ee1ea7a4de0463580aa3ad941603f7b
Natalia <lisnatalia2002.lnmc@gmail.com>
2022-02-15 13:07:30 +0100

    cambios en registro otra vez

--- app/src/main/java/institute/immune/e_parte/RegisterActivity.java
+++ app/src/main/java/institute/immune/e_parte/RegisterActivity.java
@@ -5,27 +5,60 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
     private MyOpenHelper db;
-    private EditText name, mail, password;
+    private EditText name, mail, password, passwordC;
+    private TextView message;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         db = new MyOpenHelper(this);
+
         bindings();
+
+        message.setVisibility(View.INVISIBLE);
+
     }
 
     public void register(View view) {
-        db.añadirUsuario(name.getText().toString(), mail.getText().toString(),password.getText().toString());
+        showMessage();
+        System.out.println(passwordC + "    " + password);
+        if (!password.getText().toString().equals(passwordC.getText().toString())){
+            setMessage("Las contraseñas no coinciden");
+        }
+        else {
+            db.añadirUsuario(name.getText().toString(), mail.getText().toString(), password.getText().toString());
+        }
+    }
 
+
+
+    public void showMessage(){
+        if (isEmpty(name)||isEmpty(mail)||isEmpty(password)||isEmpty(passwordC)){
+            setMessage("Por favor rellena todos los campos");
+        }
+    }
+
+    public void setMessage(String text){
+        message.setVisibility(View.VISIBLE);
+        message.setText(text);
+    }
+
+
+    public boolean isEmpty(EditText x){
+        return x.getText().toString().isEmpty();
     }
 
     public void bindings(){
             name = findViewById(R.id.inputNameR);
             mail = findViewById(R.id.inputMailR);
             password = findViewById(R.id.inputPasswordR);
+            passwordC = findViewById(R.id.inputCPasswordR);
+            message = findViewById(R.id.txtMessage);
     }
 
     
