62ab3651170780dd9319068a4540466ccc71e1a8
Dani <daniel.gonzalez@immune.institute>
2022-02-16 19:56:54 +0100

    Boton Launcher

--- app/src/main/java/institute/immune/e_parte/Launcher.java
+++ app/src/main/java/institute/immune/e_parte/Launcher.java
@@ -2,13 +2,20 @@ package institute.immune.e_parte;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Launcher extends AppCompatActivity {
-
+    private Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
+    }
+
+    public void openHome(View view) {
+        intent = new Intent(this,Home.class);
+        startActivity(intent);
     }
 }
